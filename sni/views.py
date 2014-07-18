4f4ceed3a1c03e20110ecf7b5d6642efacce9969
Bitstein <michael@bitstein.org>
2014-07-18 14:13:45 -0500

    fix name

--- sni/views.py
+++ sni/views.py
@@ -254,7 +254,7 @@ def atomfeed():
     app.logger.info( 'IP: "' + str(request.remote_addr) + '", page:"atomfeed"')
     feed = AtomFeed('Mempool | Satoshi Nakamoto Institute',
                     feed_url=request.url, url=request.url_root)
-    articles = BlogPost.query.order_by(desc(BlogPost.date)).all()
+    articles = BlogPost.query.order_by(desc(BlogPost.added)).all()
     for article in articles:
         articleurl = url_for('blogpost', slug=article.slug, _external=True)
         content = article.excerpt + "<br><br><a href='"+articleurl+"'>Read more...</a>"
@@ -262,7 +262,7 @@ def atomfeed():
                  content_type='html',
                  author=article.author[0].first + ' ' + article.author[0].last,
                  url=articleurl,
-                 updated=article.date,
+                 updated=article.added,
                  published=article.date)
     return feed.get_response()
 
