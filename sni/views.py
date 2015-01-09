9780ebc8c64ed97a9f94aa81c4e5cd02749e0987
Michael Goldstein <michael@bitstein.org>
2015-01-08 23:53:18 -0600

    fix views

--- sni/views.py
+++ sni/views.py
@@ -8,55 +8,13 @@
 from sni import app, db, cache
 from models import Post, Email, Doc, ResearchDoc, Author, Format, Category,\
                    BlogPost, Skeptic, DonationAddress, Episode
-from flask import render_template, json, url_for, redirect, request, Response, send_file
+from flask import render_template, json, url_for, redirect, request, Response
 from sqlalchemy import desc
 from werkzeug.contrib.atom import AtomFeed
 from datetime import datetime
-import re, mimetypes, os
+import re
 
 from jinja2 import evalcontextfilter, Markup, escape
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Accept-Ranges', 'bytes')
-    return response
- 
- 
-def send_file_partial(path):
-    """ 
-        Simple wrapper around send_file which handles HTTP 206 Partial Content
-        (byte ranges)
-        TODO: handle all send_file args, mirror send_file's error handling
-        (if it has any)
-    """
-    range_header = request.headers.get('Range', None)
-    if not range_header: return send_file(path)
-    
-    size = os.path.getsize(path)    
-    byte1, byte2 = 0, None
-    
-    m = re.search('(\d+)-(\d*)', range_header)
-    g = m.groups()
-    
-    if g[0]: byte1 = int(g[0])
-    if g[1]: byte2 = int(g[1])
- 
-    length = size - byte1
-    if byte2 is not None:
-        length = byte2 - byte1
-    
-    data = None
-    with open(path, 'rb') as f:
-        f.seek(byte1)
-        data = f.read(length)
- 
-    rv = Response(data, 
-        206,
-        mimetype=mimetypes.guess_type(path)[0], 
-        direct_passthrough=True)
-    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
- 
-    return rv
 
 @app.errorhandler(404)
 def internal_error(error):
