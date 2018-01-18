d4863aabba6c0b7d9ffeca156e006c541d70cfc3
Michael Goldstein <michael@bitstein.org>
2018-01-17 18:03:40 -0600

    use short date format for persian

--- sni/util/filters.py
+++ sni/util/filters.py
@@ -6,4 +6,6 @@ from .. import app
 @app.template_filter()
 def dateformat(d, date_format="long", lang="en"):
     """Print a date in a given locale."""
+    if lang == 'fa':
+        return format_date(d, "short", lang)
     return format_date(d, date_format, lang)
