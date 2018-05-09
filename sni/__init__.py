4ec9eb7eea30d8999904e4681049c4e13b8b53bd
Michael Goldstein <michael@bitstein.org>
2018-05-09 13:34:37 -0500

    __init__.py: Add ReversedProxied to handle internal HTTPS requests

--- sni/__init__.py
+++ sni/__init__.py
@@ -14,8 +14,21 @@ from flask_flatpages import FlatPages
 import jinja2
 from datetime import datetime
 
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
 app.config.from_object('config')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 # Include docs folder in templates
 my_loader = jinja2.ChoiceLoader([
