16e91ab7376f2b1e37c56542cb987a527848bb6c
Michael Goldstein <michael@bitstein.org>
2021-12-22 21:39:38 -0600

    Update to coinmetrics api v4

--- fetch_prices.py
+++ fetch_prices.py
@@ -18,13 +18,14 @@ def update_skeptics():
     for skeptic in skeptics:
         date_diff = (prices[-1].date - skeptic.date).days + 1  # Include first day
         filtered_prices = price_query.filter(Price.date >= skeptic.date).all()
-        total_btc = Decimal('0')
+        total_btc = Decimal("0")
         for price_data in filtered_prices:
-            btc = round(Decimal('1') / Decimal(price_data.price), 8)
+            btc = round(Decimal("1") / Decimal(price_data.price), 8)
             total_btc += btc
         usd_value = round(total_btc * Decimal(prices[-1].price), 2)
         percent_change = round(
-            ((Decimal(usd_value) - Decimal(date_diff)) / Decimal(date_diff)) * 100, 2)
+            ((Decimal(usd_value) - Decimal(date_diff)) / Decimal(date_diff)) * 100, 2
+        )
         skeptic._btc_balance = str(total_btc)
         skeptic._usd_invested = date_diff
         skeptic._usd_value = str(usd_value)
@@ -35,19 +36,19 @@ def update_skeptics():
 
 
 def main():
-    url = 'https://community-api.coinmetrics.io/v2/assets/btc/metricdata?metrics=PriceUSD'
+    url = "https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&metrics=PriceUSD,SplyCur&frequency=1d&start_time="
     prices = Price.query.all()
 
     if prices:
         latest = prices[-1]
         start_date = latest.date + timedelta(days=1)
-        url = f'{url}&start={start_date}'
+        url = f"{url}{start_date}"
 
     resp = requests.get(url).json()
-    series = resp['metricData']['series']
+    series = resp["data"]
     for se in series:
-        date = parser.parse(se['time'])
-        price = se['values'][0]
+        date = parser.parse(se["time"])
+        price = se["PriceUSD"]
         new_price = Price(
             date=date,
             price=price,
