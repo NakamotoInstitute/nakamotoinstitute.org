fe799f7cd15ec7a4372207aa764b67f01d6f3daf
Michael Goldstein <michael@bitstein.org>
2023-10-10 16:18:35 -0500

    Move server files into project subdirectory

--- server/app/cli/utils/json.py
+++ server/app/cli/utils/json.py
@@ -1,15 +1,21 @@
 import json
 
-from pydantic import BaseModel, ValidationError
+from pydantic import (
+    BaseModel,
+    ValidationError,
+)
 
 
-def load_and_validate_json(json_file: str, item_schema: BaseModel):
+def load_and_validate_json(
+    json_file: str,
+    item_schema: BaseModel,
+):
     try:
         with open(json_file, "r") as file:
             data = json.load(file)
             if not isinstance(data, list):
                 print(
-                    f"Invalid data format. Expected a list but got {type(data).__name__}"
+                    f"Invalid data format. Expected a list but got {type(data).__name__}"  # noqa: E501
                 )
                 return None
 
