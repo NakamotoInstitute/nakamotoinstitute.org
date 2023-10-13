f291977297981ece455ea9b20a9ebc3813972568
Michael Goldstein <michael@bitstein.org>
2023-10-13 17:14:18 -0500

    Change api base url

--- client/lib/api/fetchAPI.ts
+++ client/lib/api/fetchAPI.ts
@@ -1,4 +1,10 @@
-const apiBaseUrl = "http://127.0.0.1:5000/api";
+const apiBaseUrl = (() => {
+  const environ = process.env.VERCEL_ENV;
+  if (environ === "development") {
+    return "http://127.0.0.1:5000";
+  }
+  return process.env.API_URL as string;
+})();
 
 export default async function fetchAPI(
   endpoint: string,
