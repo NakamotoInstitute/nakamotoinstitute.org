775c909466248e7cff4c8b5ac189d7b5c708a3d0
Michael Goldstein <michael@bitstein.org>
2023-12-05 13:43:44 -0600

    Update packages

--- server/migrations/versions/7879d3c49f28_initialize_database.py
+++ server/migrations/versions/7879d3c49f28_initialize_database.py
@@ -5,8 +5,8 @@ Revises:
 Create Date: 2023-11-16 19:46:47.773776
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
