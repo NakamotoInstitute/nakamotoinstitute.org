6cd7291a39334993fbecdf0dee96e372b3494cce
Michael Goldstein <michael@bitstein.org>
2024-02-12 14:43:45 -0600

    Separate get_category_quotes

--- server/sni/satoshi/quotes/router.py
+++ server/sni/satoshi/quotes/router.py
@@ -18,8 +18,10 @@ async def get_quote_categories(db: AsyncSession = Depends(get_db)):
 
 @router.get("/{slug}", response_model=QuoteCategoryModel)
 async def get_quote_category(slug: str, db: AsyncSession = Depends(get_db)):
-    category = await service.get(slug, db_session=db)
+    category = await service.get_category(slug, db_session=db)
     if not category:
         raise HTTPException(status_code=404, detail="Category not found")
 
-    return {"category": category, "quotes": category.quotes}
+    quotes = await service.get_category_quotes(category.slug, db_session=db)
+
+    return {"category": category, "quotes": quotes}
--- server/sni/satoshi/quotes/service.py
+++ server/sni/satoshi/quotes/service.py
@@ -7,20 +7,27 @@ from sqlalchemy.orm import joinedload, selectinload
 from sni.models import Email, ForumPost, Quote, QuoteCategory
 
 
-async def get(slug: str, *, db_session: AsyncSession) -> QuoteCategory:
-    query = (
-        select(QuoteCategory)
-        .options(
-            selectinload(QuoteCategory.quotes).options(
-                joinedload(Quote.email).joinedload(Email.thread),
-                joinedload(Quote.post).joinedload(ForumPost.thread),
-                selectinload(Quote.categories),
-            )
-        )
-        .filter_by(slug=slug)
-    )
+async def get_category(slug: str, *, db_session: AsyncSession) -> QuoteCategory:
+    query = select(QuoteCategory).filter_by(slug=slug)
 
     return await db_session.scalar(query)
+
+
+async def get_category_quotes(slug: str, *, db_session: AsyncSession) -> List[Quote]:
+    query = (
+        select(Quote)
+        .options(
+            joinedload(Quote.email).joinedload(Email.thread),
+            joinedload(Quote.post).joinedload(ForumPost.thread),
+            selectinload(Quote.categories),
+        )
+        .join(QuoteCategory, Quote.categories)
+        .filter(QuoteCategory.slug == slug)
+        .order_by(Quote.date)
+    )
+
+    result = await db_session.scalars(query)
+    return result.all()
 
 
 async def get_all(*, db_session: AsyncSession) -> List[QuoteCategory]:
