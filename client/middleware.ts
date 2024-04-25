7edd67ae24777db8b243b850d8a46a18c7e99272
Michael Goldstein <michael@bitstein.org>
2024-04-25 16:16:00 -0500

    Improve middleware matcher

--- client/middleware.ts
+++ client/middleware.ts
@@ -44,9 +44,8 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips the
-  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
+  // Skip all paths that should not be localized.
   matcher: [
-    "/((?!api/|_next/static|_next/image|favicon.ico|img/|static/|satoshinakamoto.asc|.well-known).*)",
+    "/((?!api/|_next/|_vercel/|\\.well-known/|favicon\\.ico$|satoshinakamoto\\.asc$).*)",
   ],
 };
