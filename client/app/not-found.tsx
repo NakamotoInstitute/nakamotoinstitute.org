cbd3ca87fb8d12049a67cd7e583be2e2665c511b
Michael Goldstein <michael@bitstein.org>
2024-05-07 09:18:36 -0500

    Add root layout to not found

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,10 +1,16 @@
 import NotFoundLayout from "@/app/components/NotFoundLayout";
 import { urls } from "@/lib/urls";
 
+import { RootLayout } from "./components/RootLayout";
+
 const generateHref = (loc: Locale) => urls(loc).home;
 
 export default function NotFound() {
   const locale: Locale = "en";
 
-  return <NotFoundLayout locale={locale} generateHref={generateHref} />;
+  return (
+    <RootLayout locale={locale}>
+      <NotFoundLayout locale={locale} generateHref={generateHref} />
+    </RootLayout>
+  );
 }
