96e66de0d93677da695df42695d9d449550ac0d8
Michael Goldstein <michael@bitstein.org>
2024-07-17 13:25:12 -0500

    Add italic-regular-em utility

--- client/app/[locale]/library/components/NodeHeader.tsx
+++ client/app/[locale]/library/components/NodeHeader.tsx
@@ -17,7 +17,10 @@ export async function NodeHeader({ t, locale, node }: NodeHeaderProps) {
         ) : null}
         <h1 className="mb-2 text-5xl font-medium">{node.title}</h1>
         {node.subheading ? (
-          <p className="mb-6 text-2xl italic">{node.subheading}</p>
+          <p
+            className="italic-regular-em mb-6 text-2xl italic"
+            dangerouslySetInnerHTML={{ __html: node.subheading }}
+          />
         ) : null}
       </header>
       <hr className="mx-auto my-6 w-12 border border-opacity-40" />
