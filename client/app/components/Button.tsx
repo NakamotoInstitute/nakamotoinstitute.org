67cd4f5018000dd03d328ef178fea558e5b049ea
Michael Goldstein <michael@bitstein.org>
2024-07-23 10:43:53 -0500

    Restyle navbar

---
+++ client/app/components/Button.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+type ButtonLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+  target?: string;
+  rel?: string;
+};
+
+export const ButtonLink = ({ href, children, className }: ButtonLinkProps) => {
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+        "h-10 bg-cardinal px-4 py-3 text-white",
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import Link from "next/link";
 import { useState } from "react";
 
+import { ButtonLink } from "./Button";
 import { LanguageToggle, ToggleLinkProps } from "./LanguageToggle";
 
 export function Navbar({
@@ -23,65 +24,62 @@ export function Navbar({
   return (
     <nav className="border-b border-dashed border-dark">
       <div className="mx-auto w-full max-w-screen-1.5xl px-4 font-bold">
-        <div className="relative flex items-center justify-between py-6">
-          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-            <button
-              id="mobileMenuButton"
-              type="button"
-              className="inline-flex items-center justify-center rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-inset"
-              aria-controls="mobile-menu"
-              aria-expanded={menuOpen}
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                id="mobileMenuIconClosed"
-                className={clsx("h-6 w-6", { hidden: menuOpen })}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden={menuOpen}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                id="mobileMenuIconOpen"
-                className={clsx("h-6 w-6", { hidden: !menuOpen })}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden={!menuOpen}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+        <div className="grid grid-cols-2 items-center gap-x-4 py-6 md:grid-cols-[1fr_auto_1fr]">
+          <Link href={homeHref}>{logo}</Link>
+          <div className="mx-auto hidden h-12 items-center md:flex">
+            {navLinks.map(({ href, text }) => (
+              <Link key={text} href={href} className="p-2 text-dark">
+                {text}
+              </Link>
+            ))}
           </div>
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <Link href={homeHref}>{logo}</Link>
-            </div>
-            <div className="hidden h-12 items-center md:ml-auto md:flex md:space-x-4">
-              {navLinks.map(({ href, text }) => (
-                <Link key={text} href={href} className="p-2 text-dark">
-                  {text}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+          <div className="ml-auto flex items-center gap-x-4">
+            <ButtonLink href="">Donate</ButtonLink>
             <LanguageToggle current={locale} {...toggleProps} />
+            <div className="flex items-center md:hidden">
+              <button
+                id="mobileMenuButton"
+                type="button"
+                className="inline-flex items-center justify-center rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-inset"
+                aria-controls="mobile-menu"
+                aria-expanded={menuOpen}
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  id="mobileMenuIconClosed"
+                  className={clsx("h-6 w-6", { hidden: menuOpen })}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden={menuOpen}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                <svg
+                  id="mobileMenuIconOpen"
+                  className={clsx("h-6 w-6", { hidden: !menuOpen })}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden={!menuOpen}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>
